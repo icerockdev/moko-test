3662ad6b6189b1979f3b26ffa0f7ea1ac81d1bad
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-02-11 19:39:51 +0700

    try to fix windows run

--- run-check.sh
+++ run-check.sh
@@ -1,6 +1,4 @@
-OS=$1
-
-if [ "$OS" == "macos-latest" ]; then
+if [ "$1" == "macos-latest" ]; then
   ./gradlew detektIosArm32Main \
     detektIosArm64Main \
     detektIosSimulatorArm64Main \
@@ -38,11 +36,11 @@ if [ "$OS" == "macos-latest" ]; then
     publishWatchosSimulatorArm64PublicationToMavenLocal \
     publishWatchosX64PublicationToMavenLocal \
     publishWatchosX86PublicationToMavenLocal
-elif [ "$OS" == "windows-latest" ]; then
+elif [ "$1" == "windows-latest" ]; then
   ./gradlew detektMingwX64Main \
     mingwX64Test \
     publishMingwX64PublicationToMavenLocal
-elif [ "$OS" == "ubuntu-latest" ]; then
+elif [ "$1" == "ubuntu-latest" ]; then
   ./gradlew detektWithoutTests build publishToMavenLocal
 else
   ./gradlew detektWithoutTests build publishToMavenLocal
