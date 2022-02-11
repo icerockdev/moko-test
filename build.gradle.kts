3122578b3cbfb4bebb568e431397bf30304cbb5c
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-02-11 16:56:42 +0700

    improve publication with nexus plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,19 @@ buildscript {
     }
 }
 
+plugins {
+    alias(libs.plugins.nexusPublish)
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("OSSRH_USER"))
+            password.set(System.getenv("OSSRH_KEY"))
+        }
+    }
+}
+
 // temporary fix for Apple Silicon (remove after 1.6.20 update)
 rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
     rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
